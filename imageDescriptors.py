5430b6978035954e6209d19ac02b6fd699ae3934
Aaron Lin <lin.aaron1@gmail.com>
2016-10-31 16:47:17 -0400

    added method for computing the descriptor using rotation as the group operator

--- imageDescriptors.py
+++ imageDescriptors.py
@@ -8,8 +8,8 @@ def showImage(image, title="Title"):
 
 
 class ImageInvariants:
-	def __init__(self, image_path, max_height=512):
-		self.image = cv2.imread(image_path)
+	def __init__(self, image_path, max_height=64):
+		self.image = cv2.imread(image_path).astype(np.float32)
 		height, width, channels = self.image.shape
 		if height>max_height:
 			self.image = cv2.resize(self.image, (max_height, width*max_height/height))
@@ -18,7 +18,7 @@ class ImageInvariants:
 		self.width = self.image.shape[1]
 
 
-	# Returns the histogram descriptor
+	# Returns rotated image
 	def rotate(self, degrees=0):
 		image = self.image
 		height = self.height
@@ -27,20 +27,34 @@ class ImageInvariants:
 
 		rotationMat = cv2.getRotationMatrix2D(center, degrees, 1)
 		rotated = cv2.warpAffine(image, rotationMat, (height, width), 
-			borderMode=cv2.BORDER_TRANSPARENT, 
+			borderMode=cv2.BORDER_CONSTANT, 
 			borderValue=1)
 		# TUDU: play around with borderMode and borderValue for different backgrounds
 
 		return rotated
 
-	def dotProduct(self, numSteps = 360)
 
+	def rotateDescriptor(self, numsteps=360):
+		descriptor = [self.dotProduct(self.rotate(deg)) for deg in range(numsteps)]
+		for d in descriptor:
+			print d
+		return descriptor
 
+	def dotProduct(self, other):
+		def L2(x):
+			return np.linalg.norm(x)
+		def L1(x):
+			return np.sum(np.abs(x))
+
+		difference = self.image-other
+		similarity = np.apply_along_axis(L2, 2, difference)
+		#print "\t", self.image, other
+		return np.mean(similarity)
 
 
 
 knight = ImageInvariants("./data/knight1.jpg")
-knight.rotationalDescriptor()
+knight.rotateDescriptor()
 
 
 """
@@ -48,5 +62,6 @@ QUESTIONS:
 
  - How sensitive is this rotation group operation to the fringes of the images?
  	What are techniques to weight the background pixels less heavily
+ - Sensitivity to different color but same shape/outline?
 
 """
